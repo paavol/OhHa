80a5c1282283fc91fc472dbaffe06250d7def205
Paavo J Lyytikainen <paavolyy@dcb-27.cs.helsinki.fi>
2012-05-14 14:26:09 +0300

    nb-projekti luotu

---
+++ Shakki/src/shakki/Shakki.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shakki;
+
+/**
+ *
+ * @author paavolyy
+ */
+public class Shakki {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
