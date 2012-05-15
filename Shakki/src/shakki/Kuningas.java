b944a5ea9d7ce7ff93e3ad0236d4ec3d8db6d777
unknown <Maijanen@Maijanen-PC.(none)>
2012-05-15 20:53:03 +0300

    hieman koodausta

--- Shakki/src/shakki/Kuningas.java
+++ Shakki/src/shakki/Kuningas.java
@@ -17,4 +17,10 @@ public class Kuningas extends Nappula {
     public void liiku() {
         
     }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+
 }
--- Shakki/src/shakki/Nappula.java
+++ Shakki/src/shakki/Nappula.java
@@ -23,4 +23,5 @@ public class Nappula {
 
     public void syö() {
     }
+
 }
