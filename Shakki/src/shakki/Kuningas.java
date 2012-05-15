8853e64e03f332022724927aa327fc409d450f1a
Paavo J Lyytikainen <paavolyy@melkki.cs.helsinki.fi>
2012-05-15 11:34:58 +0300

    muokkauksia

---
+++ Shakki/src/shakki/Kuningas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shakki;
+
+/**
+ *
+ * @author paavolyy
+ */
+public class Kuningas extends Nappula {
+
+    public Kuningas(int x, int y) {
+        super(x, y);
+    }
+
+    public void liiku() {
+        
+    }
+}
---
+++ Shakki/src/shakki/Nappula.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shakki;
+
+/**
+ *
+ * @author paavolyy
+ */
+public class Nappula {
+
+    private int x;
+    private int y;
+
+    public Nappula(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void liiku(int x, int y) {
+    }
+
+    public void syö() {
+    }
+}
